9428cfecb8744ba98125e32fb51e31a689132a1f
Richard Schmidt <hangstrap@gmail.com>
2016-07-13 21:47:44 +1200

    Adding unit tests

---
+++ test/timezone_browser_test.dart
@@ -0,0 +1,27 @@
+@TestOn( 'browser')
+import 'package:test/test.dart';
+import 'package:timezone/browser.dart';
+import 'package:timezone/timezone.dart';
+
+main() async {
+  await initializeTimeZone();
+  test( "check some timezones ", (){
+    expect( timeZoneDatabase.locations.containsKey( "Pacific/Auckland"), true);
+    expect( timeZoneDatabase.locations.containsKey( "Africa/Johannesburg"), true);
+    expect( timeZoneDatabase.locations.containsKey( "Africa/Poffadder"), false);
+  });
+  test("match no daylight savings", () {
+    Location auckland = timeZoneDatabase.get("Pacific/Auckland");
+    DateTime utc = new DateTime.utc(2016, 05, 04, 09, 10);
+    TZDateTime aucklandTime = new TZDateTime.from(utc, auckland);
+    expect(
+        aucklandTime.toIso8601String(), equals("2016-05-04T21:10:00.000+1200"));
+  });
+  test("match  daylight savings", () {
+    Location auckland = timeZoneDatabase.get("Pacific/Auckland");
+    DateTime utc = new DateTime.utc(2016, 03, 03, 09, 10);
+    TZDateTime aucklandTime = new TZDateTime.from(utc, auckland);
+    expect(
+        aucklandTime.toIso8601String(), equals("2016-03-03T22:10:00.000+1300"));
+  });
+}
