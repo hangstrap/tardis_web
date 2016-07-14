53459f6f7cf29ef57eb086d12a3e272f69f07897
Richard Schmidt <hangstrap@gmail.com>
2016-07-14 21:37:36 +1200

    wip

--- test/custom_html_test.dart
+++ test/custom_html_test.dart
@@ -4,7 +4,14 @@ import 'dart:html';
 import 'package:test/test.dart';
 
 main( ){
+  setUp((){
+    SelectElement options = new SelectElement();
+    options.id = 'addNewTimezone';
+    querySelector('body').children.add( options);
+  });
   test( "find select", (){
+
+
         SelectElement selectAddTimezone = querySelector('#addNewTimezone');
         expect( selectAddTimezone, isNotNull);
   });
--- test/web_timezone_test.dart
+++ test/web_timezone_test.dart
@@ -9,17 +9,28 @@ import '../web/timezone.dart' as underTest;
 
 @TestOn( 'browser')
 main () async{
+
   await initializeTimeZone();
+
+  setUp((){
+    SelectElement options = new SelectElement();
+    options.id = 'addNewTimezone';
+    querySelector('body').children.add( options);
+  });
+
+
   test("Should convert UTC time to Local", (){
 
     var utc = new DateTime.utc(2016, 03, 03, 09, 10);
     var result = underTest.toLocalTime("Pacific/Auckland", utc);
     expect( result.toIso8601String(), "2016-03-03T22:10:00.000+1300");
   });
+
   test( "should convert local to UTC", (){
     var result = underTest.toUtc( "Pacific/Auckland", 2016, 3,3 ,9, 10);
     expect( result.toIso8601String(), "2016-03-02T20:10:00.000Z");
   });
+
   test( "load selector with timezone", (){
     SelectElement selectAddTimezone = querySelector('#addNewTimezone');
     expect( selectAddTimezone,  isNotNull);
