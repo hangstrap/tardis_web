3e1a6c89bec96bf43f47feb2647f3bb6eec616de
Richard Schmidt <hangstrap@gmail.com>
2016-07-18 21:02:27 +1200

    unit test

--- test/timezone_test.dart
+++ test/timezone_test.dart
@@ -1,37 +1,33 @@
-
 import 'dart:html';
 import 'package:test/test.dart';
 import 'package:timezone/browser.dart';
 
-
 import '../web/timezone.dart' as underTest;
 
-@TestOn( 'browser')
-main () async{
-
+@TestOn('browser')
+main() async {
   await initializeTimeZone();
-  test("Should convert UTC time to Local", (){
-
+  test("Should convert UTC time to Local", () {
     var utc = new DateTime.utc(2016, 03, 03, 09, 10);
     var result = underTest.toLocalTime("Pacific/Auckland", utc);
-    expect( result.toIso8601String(), "2016-03-03T22:10:00.000+1300");
+    expect(result.toIso8601String(), "2016-03-03T22:10:00.000+1300");
   });
 
-  test( "should convert local to UTC", (){
-    var result = underTest.toUtc( "Pacific/Auckland", 2016, 3,3 ,9, 10);
-    expect( result.toIso8601String(), "2016-03-02T20:10:00.000Z");
+  test("should convert local to UTC", () {
+    var result = underTest.toUtc("Pacific/Auckland", 2016, 3, 3, 9, 10);
+    expect(result.toIso8601String(), "2016-03-02T20:10:00.000Z");
   });
-  group( "load elements", (){
-      List<OptionElement> elements = underTest.loadElements();
-  test( "should have lots of element", (){
-    expect( elements.length, greaterThan( 100));
+  group("load elements", () {
+    List<OptionElement> elements = underTest.loadElements();
+    test("should have lots of element", () {
+      expect(elements.length, greaterThan(100));
+    });
+    test("first one should be UTC", () {
+      expect(elements[0].value, "UTC");
+    });
+    test("should contain Auckland", () {
+      expect(
+          elements.firstWhere((e) => e.value == "Pacific/Auckland"), isNotNull);
+    });
   });
-    test( "first one should be UTC", (){
-      expect( elements[0].value, "UTC");
-  });
-  test( "should contain Auckland", (){
-//    expect( elements, contains( )));
-  }, skip:"no idea how to do this!");
-
-});
 }
